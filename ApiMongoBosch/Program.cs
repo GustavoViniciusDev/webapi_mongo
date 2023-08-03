b9ac7c72bc16456186c40ba537edfff6a91f7a18
GustavoViniciusDev <gustavo.rocco.02@gmail.com>
2023-08-03 16:55:34 -0300

    primeiro commit

---
+++ ApiMongoBosch/Program.cs
@@ -0,0 +1,43 @@
+
+using ApiMongoBosch.Models;
+using ApiMongoBosch.Services;
+
+namespace ApiMongoBosch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.Configure<BookStoreDatabaseSettings>(
+                builder.Configuration.GetSection("BookStoreDatabase"));
+
+            builder.Services.AddSingleton<BooksService>();
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
